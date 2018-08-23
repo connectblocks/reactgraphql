bb22af0693dcda2cea05648dfcd72203c5a8ea96
michael <@>
2018-08-22 23:33:45 -0700

    implemented first schema. load graphiql

---
+++ schema/schema.js
@@ -0,0 +1,36 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+    {id:'23', firstName: 'michael', age: 18 },
+    {id:'45', firstName: 'helen', age: 16 }
+]
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: {type: GraphQLString },
+        firstName: {type: GraphQLString },
+        age: {type: GraphQLInt },
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return _.find(users, {id: args.id });
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
+
